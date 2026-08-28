23f0bd47b798f6558e00c18e782d69d2144a7277
Kunal Kad <kadkunal4@gmail.com>
2026-08-28 12:51:22 +0530

    initial commit

---
+++ program1.c
@@ -0,0 +1,29 @@
+//Program to Demonstrate Basic I/O Functions Using Different Data Types//
+#include <stdio.h>
+int main() {
+    int age;
+    float height;
+    double salary;
+    char grade;
+
+    printf("Enter your age: ");
+    scanf("%d" , &age);
+
+    printf("Enter your height: ");
+    scanf("%f" , &height);
+
+    printf("Enter your salary: ");
+    scanf("%lf" , &salary);
+
+    printf("Enter your grade: ");
+    scanf(" %c" , &grade);
+
+    printf("\nDetails\n");
+
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n" , height);
+    printf("Salary: %.2f\n" , salary);
+    printf("Grade: %c\n" , grade);
+
+    return 0;
+}
