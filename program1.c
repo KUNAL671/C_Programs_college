ae3158bef7ab5a45c9c36edbdcc1f42a77b76cbb
Kunal Kad <kadkunal4@gmail.com>
2026-08-28 18:17:56 +0530

    Program2

--- program1.c
+++ program1.c
@@ -1,4 +1,5 @@
 //Program to Demonstrate Basic I/O Functions Using Different Data Types//
+
 #include <stdio.h>
 int main() {
     int age;
---
+++ program2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    float radius,area;
+    const float pi=3.14159;
+
+    printf("Enter the area of the circle: ");
+    scanf("%f",&radius);
+
+    area = pi * radius * radius;
+
+    printf("Area of the circle is: %.2f\n",area);
+
+    return 0;
+}
